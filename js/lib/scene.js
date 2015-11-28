b491e48ec664cd37ca4ab6480786c868d4249fbe
Clonkonaut <Armag3ddon@web.de>
2015-11-28 11:00:18 +0100

    Added update, draw, click, mousedown and mouseup to Scene prototype.

--- js/lib/scene.js
+++ js/lib/scene.js
@@ -1,14 +1,36 @@
 function Scene() {
-	this.position = new V2(0, 0);
-	this.size = new V2(0, 0);
 	this.entities = [];
-
-	var oldDraw = this.draw;
-	this.draw = function(ctx ) {
-		if( this.bg )
-			this.bg.draw( ctx, 0 ,0 );
-		oldDraw( ctx );
-	};
 }
 
-Scene.prototype = new Entity();
+Scene.prototype.update = function( delta ) {
+	for( var i = 0; i < this.entities.length; i++ )
+		if( this.entities[i].update )
+			this.entities[i].update( delta );
+};
+
+Scene.prototype.draw = function( ctx ) {
+	if( this.bg )
+		this.bg.draw( ctx, 0 ,0 );
+
+	for( var i = 0; i < this.entities.length; i++ )
+		if( this.entities[i].draw )
+			this.entities[i].draw( ctx );
+};
+
+Scene.prototype.click = function( pos ) {
+	for( var i = 0; i < this.entities.length; i++ )
+		if( this.entities[i].click )
+			this.entities[i].click( pos );
+};
+
+Scene.prototype.mousedown = function( pos ) {
+	for( var i = 0; i < this.entities.length; i++ )
+		if( this.entities[i].mousedown )
+			this.entities[i].mousedown( pos );
+};
+
+Scene.prototype.mouseup = function( pos ) {
+	for( var i = 0; i < this.entities.length; i++ )
+		if( this.entities[i].mouseup )
+			this.entities[i].mouseup( pos );
+};
