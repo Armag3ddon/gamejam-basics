72a244f0da3ecd82afe400a911e87b6e2e743aac
Clonkonaut <Armag3ddon@web.de>
2015-12-02 19:33:46 +0100

    Added rand(min, max), random function.

--- js/lib/util.js
+++ js/lib/util.js
@@ -10,6 +10,10 @@ Framecounter.prototype.update = function( delta ) {
 };
 
 function shuffle(o){
-    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
+	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+}
+
+function rand( min, max ) {
+	return min+Math.round(Math.random()*(max-min));
 }
